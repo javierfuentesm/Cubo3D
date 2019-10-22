50abe032c2be0ed32c2a37dc238b85669d082b85
rjfm <javier.fuentesm@hotmail.com>
2019-10-22 09:41:42 -0500

    Se termian el cubo en 3d

---
+++ utils.js
@@ -0,0 +1,50 @@
+import MatrixMath from 'react-native/Libraries/Utilities/MatrixMath';
+
+export const rotateXY = (dx, dy) => {
+  const radX = (Math.PI / 180) * dy;
+  const cosX = Math.cos(radX);
+  const sinX = Math.sin(radX);
+
+  const radY = (Math.PI / 180) * -dx;
+  const cosY= Math.cos(radY);
+  const sinY = Math.sin(radY);
+  
+
+  return [
+    cosY, sinX * sinY, cosX * sinY, 0,
+    0, cosX, -sinX, 0,
+    -sinY, cosY * sinX, cosX * cosY, 0,
+    0, 0, 0, 1
+  ];
+};
+console.disableYellowBox = true;
+
+export const rotateXZ = (dx, dy) => {
+  const radX = (Math.PI / 180) * dx;
+  const cosX = Math.cos(radX);
+  const sinX = Math.sin(radX);
+
+  const radY = (Math.PI / 180) * dy;
+  const cosY= Math.cos(radY);
+  const sinY = Math.sin(radY);
+
+  return [
+    cosX, -cosY * sinX, sinX * sinY, 0,
+    sinX, cosX * cosY, -sinY * cosX, 0,
+    0, sinY, cosY, 0,
+    0, 0, 0, 1
+  ];
+};
+
+//source: https://gist.github.com/jmurzy/0d62c0b5ea88ca806c16b5e8a16deb6a#file-foldview-transformutil-transformorigin-js
+export const transformOrigin = (matrix, origin) => {
+  const { x, y, z } = origin;
+
+  const translate = MatrixMath.createIdentityMatrix();
+  MatrixMath.reuseTranslate3dCommand(translate, x, y, z);
+  MatrixMath.multiplyInto(matrix, translate, matrix);
+
+  const untranslate = MatrixMath.createIdentityMatrix();
+  MatrixMath.reuseTranslate3dCommand(untranslate, -x, -y, -z);
+  MatrixMath.multiplyInto(matrix, matrix, untranslate);
+};
